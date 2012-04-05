8b8e3531c2ec0483019e5d207ea9b547b3d2fb15
Edison Wang <wzsddtc@gmail.com>
2012-04-04 23:31:57 -0400

    init

---
+++ src/com/edisonwang/skillshare/slm/WhereAreMyFriendsActivity.java
@@ -0,0 +1,13 @@
+package com.edisonwang.skillshare.slm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhereAreMyFriendsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
